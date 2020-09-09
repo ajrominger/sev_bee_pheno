2a13eda8040cf15bd5376e7a165deefee783c0fd
ajrominger <ajrominger@gmail.com>
2020-09-09 15:22:45 -0400

    new test case

--- R/gaus_negb.R
+++ R/gaus_negb.R
@@ -33,14 +33,12 @@ fitGausNB <- function(x, y) {
 
 
 
-aa <- c(40, 0.5, 1)
-k <- 10
+aa <- c(40, 2.5, 1)
+k <- 0.1
 x <- rep(3:10, 25) - 6.5
 yhat <- gaus(x, aa[1], aa[2], aa[3])
 y <- rnbinom(length(x), size = k, mu = yhat)
 plot(x, y)
-
-gausNBLogLik(c(aa, k), x, y)
 
 fitGausNB(x, y)
 
